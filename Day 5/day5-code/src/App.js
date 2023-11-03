3023990395129977f5ae66785b76f7999031f8bf
Ananthu J P <31985570+ananthujp@users.noreply.github.com>
2023-11-03 12:41:42 +0530

    Update App.js

--- Day 5/day5-code/src/App.js
+++ Day 5/day5-code/src/App.js
@@ -1,24 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ConditionalRender from "./ConditionalRender";
+import ListMap from "./ListMap";
+import StateExample from "./StateExample";
+import EffectExample from "./EffectExample";
+import EffectExample2 from "./EffectExample2";
+import CustomHookExample from "./CustomHook/CustomHookExample";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ConditionalRender />
+      {/* <ListMap/>
+  <StateExample/>
+  <EffectExample/>
+  <EffectExample2/>
+  <CustomHookExample/> */}
+    </>
   );
 }
 
