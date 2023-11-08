3b11fc6abd00b8baa0933688f575aa9a65c1093c
Chandrabhan <chandrabhanpatel321@gmail.com>
2023-11-09 00:15:00 +0530

    Uploaded again

---
+++ Projects/Chandrabhan/simple-clock/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Clock from './Clock';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Clock</h1>
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
